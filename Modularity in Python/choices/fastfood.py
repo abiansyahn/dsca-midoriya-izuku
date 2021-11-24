a440d4bac0fbff66d87f0d9a6056ae3c65adc59f
abiansyahn <91464618+abiansyahn@users.noreply.github.com>
2021-11-24 13:51:25 +0700

    Delete Modularity in Python/choices directory

--- Modularity in Python/choices/fastfood.py
+++
@@ -1,8 +0,0 @@
-from random import choice
-
-fastfood = ["McDonalds", "KFC", "Burger King", "Wendys",
-            "Pizza Hut", "Toby's", "Lawless", "CFC"]
-
-def pick():
-    """Return random fast food place."""
-    return choice(fastfood)
